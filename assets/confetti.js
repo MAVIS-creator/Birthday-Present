5d5f2283696a07d2fb7c50b01c38f831f53f7191
MAVIS-creator <akintunde.dolapo1@gmail.com>
2025-08-16 19:27:00 +0100

    Auto-save changes

---
+++ assets/confetti.js
@@ -0,0 +1,47 @@
+ // Run confetti when page loads
+    window.onload = () => {
+      let duration = 3 * 1000; // 3 seconds
+      let end = Date.now() + duration;
+
+      (function frame() {
+        confetti({
+          particleCount: 5,
+          angle: 60,
+          spread: 55,
+          origin: { x: 0 }
+        });
+        confetti({
+          particleCount: 5,
+          angle: 120,
+          spread: 55,
+          origin: { x: 1 }
+        });
+
+        if (Date.now() < end) {
+          requestAnimationFrame(frame);
+        }
+      })();
+    }; // Run confetti when page loads
+    window.onload = () => {
+      let duration = 3 * 1000; // 3 seconds
+      let end = Date.now() + duration;
+
+      (function frame() {
+        confetti({
+          particleCount: 5,
+          angle: 60,
+          spread: 55,
+          origin: { x: 0 }
+        });
+        confetti({
+          particleCount: 5,
+          angle: 120,
+          spread: 55,
+          origin: { x: 1 }
+        });
+
+        if (Date.now() < end) {
+          requestAnimationFrame(frame);
+        }
+      })();
+    };
